7c35879a27379088f48e01a503b106a3e24b1c21
Dave Allie <david.allie@optusnet.com.au>
2015-05-28 20:06:51 +1000

    Cleaned up mixed whitespace characters and blank lines with whitespace

--- import_times.js
+++ import_times.js
@@ -1,33 +1,33 @@
 
 $(function() {
-       
-	var subject;  // hold all the imported times
-	
+
+    var subject;  // hold all the imported times
+
     /**
      * Shows a 'Launch Timetabler' and adds the import button for later use
      */
     function showImportButton() {
         $('.qv_table').append('<button class="launchButton">Launch Timetabler</button>');
-        $('.qv_table').append('<button style="display:none;" class="importButton">Classes Found. Import?</button>');    
+        $('.qv_table').append('<button style="display:none;" class="importButton">Classes Found. Import?</button>');
     }
-    
 
-    /** 
+
+    /**
      * Handles the importing of class times. Grabs the data from the table and sends
      * it to the timetabler page
      */
     function importClasses(table) {
-        
+
         // Get the unit ID from above the table
         var unit = $.trim($(table).prev().prev().find('strong').html()).split(' ')[0];
-        
+
         // Create a new array for the unit
-		subject = { 'unit': unit, 'subject': $.trim($(table).prev().prev().find('strong').html()) };
-		subject['times'] = [];
-		
-		// Loop through table and get times
+        subject = { 'unit': unit, 'subject': $.trim($(table).prev().prev().find('strong').html()) };
+        subject['times'] = [];
+
+        // Loop through table and get times
         $(table).find('tr:not(:first-child)').each(function (index, element) {
-            
+
             // Construct an object containing the row information
             var rows = {
                 'subject_name': subject.subject,
@@ -45,35 +45,35 @@ $(function() {
             }
 
             // Push it in
-			subject['times'].push(rows);
-			
-        });        
-		
+            subject['times'].push(rows);
+
+        });
+
         fireCompletionMessage(unit);
     }
 
     function fireCompletionMessage(unit) {
-	   
+
         // Trigger import complete method in timetable_launcher.js
         chrome.runtime.sendMessage({type: "importComplete", unit: unit, class_info: JSON.stringify(subject)}, function(response) {
           console.log(response)
         });
 
     }
-    
-    
+
+
     // Add the import button to the table
     showImportButton();
-    
+
     // Add an event listener for the import button
     $('button.importButton').bind('click', function() {
         importClasses($(this).parent());
     });
-    
+
     // Add an event listener for the launch timetabler button
     $('button.launchButton').bind('click', function() {
-        
-        // Initialize the timetabler 
+
+        // Initialize the timetabler
         chrome.runtime.sendMessage({type: "init"}, function(response) {
           console.log(response)
         });
@@ -86,5 +86,5 @@ $(function() {
         $('.importButton').show();
 
     });
-    
+
 });
--- timetable_launcher.js
+++ timetable_launcher.js
@@ -3,11 +3,11 @@ var class_info;
 
 
 /**
- * Open up the timetabler application or if it's already open,  
- * bring the user to it 
+ * Open up the timetabler application or if it's already open,
+ * bring the user to it
  */
 function openOrFocusOptionsPage() {
-   var optionsUrl = chrome.extension.getURL('timetabler.html'); 
+   var optionsUrl = chrome.extension.getURL('timetabler.html');
    chrome.tabs.query({}, function(extensionTabs) {
       var found = false;
       for (var i=0; i < extensionTabs.length; i++) {
@@ -23,82 +23,79 @@ function openOrFocusOptionsPage() {
    });
 }
 
-/** 
+/**
  * Called when the user clicks on the browser action icon (the extension icon in the address bar)
- * NOTE: Might get rid of this... Seems kinda pointless 
+ * NOTE: Might get rid of this... Seems kinda pointless
  */
 chrome.browserAction.onClicked.addListener(function(tab) {
    openOrFocusOptionsPage();
 });
 
 
-/** 
+/**
  * Triggers a chrome notification so the user can see
  * that their class times have been imported. Clicking the
- * notification will open up the timetabler 
+ * notification will open up the timetabler
  */
 function notify(unit) {
 
-	// Check their bowser can handle notifications
-	if (!('Notification' in window)) { 
-
-		// this browser doesn't support the web notifications API. Just open up the timetabler view for them
-		openOrFocusOptionsPage();
-	
-	} else {
-		
-		/* NOTE: Too lazy to work this out. It shows duplicate notifications (annoying), likely 
-				because this script is loaded within the timetabler & the content script.
-				 For now, I'm just making it open up the timetabler. Will fix later. */
-		/*title = 'Class times for '+unit+' imported!';
+    // Check their bowser can handle notifications
+    if (!('Notification' in window)) {
+        // this browser doesn't support the web notifications API. Just open up the timetabler view for them
+        openOrFocusOptionsPage();
+    } else {
+        /* NOTE: Too lazy to work this out. It shows duplicate notifications (annoying), likely
+        because this script is loaded within the timetabler & the content script.
+        For now, I'm just making it open up the timetabler. Will fix later. */
+        /*title = 'Class times for '+unit+' imported!';
         options = { body: 'Click here to start planning.' };
 
-		notification = Notification.requestPermission(function() {
+        notification = Notification.requestPermission(function() {
             var notification = new Notification(title, options);
 
- 			notification.onclick = function() {
- 				console.log(class_info);
-				openOrFocusOptionsPage();
-			};
-      	});*/
+            notification.onclick = function() {
+            console.log(class_info);
+            openOrFocusOptionsPage();
+        };
+    });*/
 
-		openOrFocusOptionsPage();
+    openOrFocusOptionsPage();
 
-	}
+  }
 
-}  
+}
 
 
-/** 
- * Adds the subject and class elements into the list on the left hand side of the timetabler page. 
+/**
+ * Adds the subject and class elements into the list on the left hand side of the timetabler page.
  */
-function updateClassTimesList() { 
-	
-	var unit = class_info.unit;
-	var subject = class_info.subject;
-	var times = class_info.times;
-	var timeEl = "";
-	
-	// Convert the times into a bunch of elements
-	for(time in times) {
-		
-		timeEl = timeEl + '<div class="class '+times[time].activity.toLowerCase()+' ui-draggable" title="['+times[time].activity+'] '+times[time].subject_name+'" activity="'+times[time].activity+'" day="'+times[time].day+'" start="'+times[time].time.start+'" end="'+times[time].time.end+'" location="'+times[time].location+'" subject="'+subject+'" style="position: relative;">';
-		timeEl = timeEl + '<b>['+times[time].activity+'] '+times[time].day+' @ '+times[time].location+ '</b><br>'+times[time].time.raw;
-		timeEl = timeEl + '</div>';
-			
-	}
-	
-	// Create a new subject element
-	el = '<div class="class_list"><h4>'+unit+'</h4><div class="classes">'+timeEl+'</div></div>';
-	
-	// Add the element to the class list in the sidebar
-	$('.class_container').append(el);
+function updateClassTimesList() {
 
-	// Notify the user that their times have been imported
-	notify(unit);
+    var unit = class_info.unit;
+    var subject = class_info.subject;
+    var times = class_info.times;
+    var timeEl = "";
 
-	// Track this with GA
-	_gaq.push(['_trackEvent', subject, 'imported'])
+    // Convert the times into a bunch of elements
+    for(time in times) {
+
+        timeEl = timeEl + '<div class="class '+times[time].activity.toLowerCase()+' ui-draggable" title="['+times[time].activity+'] '+times[time].subject_name+'" activity="'+times[time].activity+'" day="'+times[time].day+'" start="'+times[time].time.start+'" end="'+times[time].time.end+'" location="'+times[time].location+'" subject="'+subject+'" style="position: relative;">';
+        timeEl = timeEl + '<b>['+times[time].activity+'] '+times[time].day+' @ '+times[time].location+ '</b><br>'+times[time].time.raw;
+        timeEl = timeEl + '</div>';
+
+    }
+
+    // Create a new subject element
+    el = '<div class="class_list"><h4>'+unit+'</h4><div class="classes">'+timeEl+'</div></div>';
+
+    // Add the element to the class list in the sidebar
+    $('.class_container').append(el);
+
+    // Notify the user that their times have been imported
+    notify(unit);
+
+    // Track this with GA
+    _gaq.push(['_trackEvent', subject, 'imported'])
 
 }
 
@@ -106,19 +103,19 @@ function updateClassTimesList() {
 
 // Listen for 'importComplete' message (triggered when classes are imported)
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if(request.type == "init") {
+    if(request.type == "init") {
 
-		// Open up the timetabler page
-    	openOrFocusOptionsPage();
-    	
+        // Open up the timetabler page
+        openOrFocusOptionsPage();
+
     } else if (request.type == "importComplete"){
-		
-		// Update the global class_info var to hold this subject now
-		class_info = JSON.parse(request.class_info);
-		
-		// Update timetable options
-		updateClassTimesList();
-    } 
+
+        // Update the global class_info var to hold this subject now
+        class_info = JSON.parse(request.class_info);
+
+        // Update timetable options
+        updateClassTimesList();
+    }
 });
 
 
@@ -128,7 +125,7 @@ _gaq.push(['_setAccount', 'UA-51599319-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
