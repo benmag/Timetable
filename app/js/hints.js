0dda474cae328fac24c3045752ecf42cfa787466
Andrew Carr <deathisunknown@gmail.com>
2018-01-06 00:51:01 +1000

    Remove unhelpful hint

--- app/js/hints.js
+++ app/js/hints.js
@@ -8,7 +8,6 @@ function loadHint() {
     "Search for a unit code or description using the search bar at the top-left and pressing 'Enter'!",
     "Use the dropdown in the search bar to select your campus when searching for classes!",
     "New to QUT? We have 3 campuses: Gardens Point (GP), Kelvin Grove (KG) and Caboolture (CB)!",
-    "These tips will show every time you refresh the page. Want to see a new one? Ctrl+R!",
     "You can search for a unit code, or a unit description using the 'unit search' box in the sidebar!"
   ];
 
